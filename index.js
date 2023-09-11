a822fce10696dc1d8133ad3a465d822af2aa0728
JunhyunCho <junhyuncho@gmail.com>
2023-09-12 01:18:05 +0900

    지도에서 마커 누르면 carousel slide 움직이고, carousel slide 움직이면 지도에서 해당 마커의 센터로 지도 이동.

--- index.js
+++ index.js
@@ -1,22 +1,101 @@
+let map;
+let tourStops;
+
+
 var myCarousel = document.querySelector('#myCarousel');
 var carousel = new bootstrap.Carousel(myCarousel, {
-    interval: 2000,
+    interval: 1000,
     wrap: false
+});
+//add eventListener to carousel
+myCarousel.addEventListener('slide.bs.carousel', function (event) {
+    //get the index of the slide
+    var index = event.to;
+    //get the slide itself
+    //console.log(event.relatedTarget.id);
+    map.panTo(tourStops[index].position);
+    
 });
 
 
 
-//google maps setup
-
-let map;
-
+const DEMO_MAP_ID = "4504f8b37365c3d0"
+/**
+ * @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+// Initialize and add the map
 async function initMap() {
-    const { Map } = await google.maps.importLibrary("maps");
-
+    // Request needed libraries.
+    const { Map, InfoWindow } = await google.maps.importLibrary("maps");
+    const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary(
+      "marker",
+    );
     map = new Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
+      zoom: 12,
+      center: { lat: 34.84555, lng: -111.8035 },
+      mapId: "4504f8b37365c3d0",
     });
-}
+    // Set LatLng and title text for the markers. The first marker (Boynton Pass)
+    // receives the initial focus when tab is pressed. Use arrow keys to
+    // move between markers; press tab again to cycle through the map controls.
+    tourStops = [
+      {
+        position: { lat: 34.8791806, lng: -111.8265049 },
+        title: "Boynton Pass",
+      },
+      {
+        position: { lat: 34.8559195, lng: -111.7988186 },
+        title: "Airport Mesa",
+      },
+      {
+        position: { lat: 34.832149, lng: -111.7695277 },
+        title: "Chapel of the Holy Cross",
+      },
+      {
+        position: { lat: 34.823736, lng: -111.8001857 },
+        title: "Red Rock Crossing",
+      },
+      {
+        position: { lat: 34.800326, lng: -111.7665047 },
+        title: "Bell Rock",
+      },
+    ];
+    // Create an info window to share between markers.
+    const infoWindow = new InfoWindow();
+  
+    // Create the markers.
+    tourStops.forEach(({ position, title }, i) => {
+      const pin = new PinElement({
+        glyph: `${i + 1}`,
+      });
+      const marker = new AdvancedMarkerElement({
+        position,
+        map,
+        title: `${i + 1}. ${title}`,
+        content: pin.element,
+      });
+  
+      // Add a click listener for each marker, and set up the info window.
+      marker.addListener("click", ({ domEvent, latLng }) => {
+        const { target } = domEvent;
+  
+        infoWindow.close();
+        infoWindow.setContent(marker.title);
+        infoWindow.open(marker.map, marker);
+        //console.log("Marker clicked", target, latLng.toJSON());
+        //move center of the map to latLng
+        map.panTo(latLng);
+        //slide carousel card to the right index
+        carousel.to(i);
+        console.log("carousel index", i);
+      });
+    });
+  }
+  
+  initMap();
 
-initMap();
+
+
+
