d8c024a36d951071179b61dbcbe7fe10ccdb4c6c
JunhyunCho <junhyuncho@gmail.com>
2023-09-11 17:24:55 +0900

    ...

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 var myCarousel = document.querySelector('#myCarousel');
 var carousel = new bootstrap.Carousel(myCarousel, {
-  interval: 2000,
-  wrap: false
+    interval: 2000,
+    wrap: false
 });
+
+
+
+//google maps setup
+
+let map;
+
+async function initMap() {
+    const { Map } = await google.maps.importLibrary("maps");
+
+    map = new Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+    });
+}
+
+initMap();
